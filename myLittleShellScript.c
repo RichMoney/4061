b1952ffa159444a24d2d6ef82504f03e778ac14a
Richard Jeffery <richmoney@gmail.com>
2012-09-26 11:21:49 -0500

    myLittleShellScript.c
    myLittleShellScript is the base shell, blueshell demonstrates string
    concat and shelltest is a random thing I wrote while exploring

---
+++ myLittleShellScript.c
@@ -0,0 +1,69 @@
+//
+//  Blue Shell.c
+//  
+//
+//  Created by Brian Salter on 9/15/12.
+//
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main (int argc, char *argv[])
+{
+    printf("Welcome to My Little Shell!  Try not to disturb the ponies.\n\n");
+    
+    int maxSize = 100;
+    char cmd[maxSize];
+    char myLittleProgram[maxSize];
+    char myLittleArguments[maxSize];
+    
+    while (69)
+    {
+        printf("Command: ");
+        fgets(cmd, maxSize, stdin);
+        
+        // Replaces the newline with a null character for printf
+        char *newline = strchr(cmd, '\n');
+        if (newline != NULL)
+            *newline = '\0';
+        
+        printf("%s%s\n", "Executing command: ", cmd);
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < maxSize && cmd[i] != ' ' && cmd[i] != '\0')
+        {
+            myLittleProgram[i] = cmd[i];
+            i++;
+        }
+        while (i < maxSize && cmd[i] != '\0')
+        {
+            myLittleArguments[j] = cmd[i];
+            i++;
+            j++;
+        }
+        
+        if (!strcmp(myLittleProgram, "exit"))
+            return 0;
+        else
+        {
+            char path[maxSize] = "/bin/";
+            strlcat(path, myLittleProgram);
+            printf(path);
+            execlp(fullPath, myLittleProgram, NULL);
+            break;
+        }
+        
+    }
+    
+    return 0;
+}
+
+char[] createPath()
+{
+    char path[];
+    
+}
