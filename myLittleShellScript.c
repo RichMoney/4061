c71505e3b4ffcf8c1c0a259f8c21cec0150cbd56
Chris Arcand <chris@chrisarcand.com>
2012-09-30 20:38:21 -0500

    Forking, uses child for multiple commands
    Now it needs background processes.

--- myLittleShellScript.c
+++ myLittleShellScript.c
@@ -1,6 +1,6 @@
 //
 //  Blue Shell.c
-//  
+//
 //
 //  Created by Brian Salter on 9/15/12.
 //
@@ -9,33 +9,35 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[])
 {
     printf("Welcome to My Little Shell!  Try not to disturb the ponies.\n\n");
-    
+
     int maxSize = 100;
     char cmd[maxSize];
     char myLittleProgram[maxSize];
     char myLittleArguments[maxSize];
     char *myLittleEnv[maxSize];
-    
+    pid_t pid;
+
     while (69)
     {
         printf("Command: ");
         fgets(cmd, maxSize, stdin);
         char myLittlePath[] = "/bin/";
-        
+
         // Replaces the newline with a null character for printf
         char *newline = strchr(cmd, '\n');
         if (newline != NULL)
             *newline = '\0';
-        
+
         printf("%s%s\n", "Executing command: ", cmd);
-        
+
         int i = 0;
         int j = 0;
-        
+
         while (i < maxSize && cmd[i] != ' ' && cmd[i] != '\0')
         {
             myLittleProgram[i] = cmd[i];
@@ -47,23 +49,31 @@ int main (int argc, char *argv[])
             i++;
             j++;
         }
-        
+
         if (!strcmp(myLittleProgram, "exit"))
            return 0;
-        
+
         else if (!strcmp(myLittleProgram, "mylittleenv"))
         {
              char * env = getenv("PATH");
              printf(env);
              continue;
         }
-             
+
         else
         {
-            strcat(myLittlePath, myLittleProgram);
-            execlp(myLittlePath, cmd, NULL);
+            pid = fork(); //create child
+            if (pid == 0) { //if child
+                strcat(myLittlePath, myLittleProgram);
+                execlp(myLittlePath, myLittleProgram, NULL);
+                exit(0);
+            }
+            else //if parent
+            {
+                waitpid(pid); //wait for child
+            }
         }
     }
-    
+
     return 0;
 }
